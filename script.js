ca0d4694aaebe0467aab9418a1a061138fdbe18e
joyce <joyce.ann.cruz@gmail.com>
2023-06-20 16:00:11 +0800

    Fix linter errors

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-let navToggle = document.getElementById("navtoggle");
-let navToggleClose = document.getElementById("navToggleClose");
-let navLinks = document.getElementsByClassName("nav-link");
+const navToggle = document.getElementById('navtoggle');
+const navToggleClose = document.getElementById('navToggleClose');
+const navLinks = document.getElementsByClassName('nav-link');
 
 function navToggles() {
-    let nav = document.getElementById("nav");
-    nav.classList.toggle("nav-on");
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('nav-on');
 }
 
-navToggle.addEventListener("click", navToggles);
-navToggleClose.addEventListener("click", navToggles);
+navToggle.addEventListener('click', navToggles);
+navToggleClose.addEventListener('click', navToggles);
 
-for (let i = 0; i < navLinks.length; i++) {
-    navLinks[i].addEventListener("click", navToggles);
-  }
+for (let i = 0; i < navLinks.length; i += 1) {
+  navLinks[i].addEventListener('click', navToggles);
+}
